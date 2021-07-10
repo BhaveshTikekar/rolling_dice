45d40817a76e4e795809946c17a5c0f95f7655ff
Bhavesh Tikekar <bhaveshtikekar19291929@gmail.com>
2021-07-10 00:23:27 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,20 +17,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // ignore: deprecated_member_use
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => TodoUi()));
-              },
-              child: Text('Todo Ui'),
-            )
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ignore: deprecated_member_use
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TodoUi()));
+                },
+                child: Text('Todo Ui'),
+              )
+            ],
+          ),
         ),
       ),
     );
